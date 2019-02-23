11b518ad121be72ad886830d28752124216f1553
David Bolin <david@bolin.at>
2019-02-23 14:55:19 -0800

    finished add item feature

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 'use strict';
 
-
-//creating new shopping items from item input
-
-//check - add and remove strikethrough
-
-
-
-
 function main() {
   $('ul').on('click', '.shopping-item-delete', function(event) {
     $(event.target).parents('li').remove();
@@ -15,6 +7,26 @@ function main() {
 
   $('ul').on('click', '.shopping-item-toggle', function(event) {
     $(event.target).parents('li').children('span').toggleClass('shopping-item__checked');
+  });
+
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    let newItemName = $('#shopping-list-entry').val();
+    if (newItemName !== '') {
+      let newHtml = `
+      <li>
+          <span class="shopping-item">${newItemName}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+             <span class="button-label">delete</span>
+           </button>
+          </div>
+      </li>`;
+      $('ul').append(newHtml);
+    }
   });
 }
 
